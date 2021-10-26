92bc35bddf40fe6e8147891cb51016dec9d23ee0
Ryan Hoff <49401240+r-hoff@users.noreply.github.com>
2021-10-26 11:38:26 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+PORT = 3000;
+
+// ROUTES
+app.get('/', function (req, res) {
+  res.send('Sever running.');
+});
+
+// LISTENER
+app.listen(PORT, function () {
+  console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.');
+});
